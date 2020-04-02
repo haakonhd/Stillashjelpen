7c7e5ce292d4f48bf02e31904bae302af65b5a97
André Hande <andre.hande@hotmail.com>
2020-04-02 12:56:33 +0200

    manifest bugfix

--- app/src/main/java/no/hiof/bo20_g28/stillashjelpen/LoginActivity.java
+++ app/src/main/java/no/hiof/bo20_g28/stillashjelpen/LoginActivity.java
@@ -18,7 +18,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         emailTextView = findViewById(R.id.emailTextView);
         passwordTextView = findViewById(R.id.passwordTextView);
