d8e806a3939de2536e104dc93e57fa480795c142
geir <geireilertsen@hotmail.com>
2020-04-02 19:06:55 +0200

    Added RegistrationActivity, firebase-stuff and updated UI.
    Login, logout and registration works, and is connected to Firebase. MainActivity checks if user is signed in. Tuned the UI to look more like the photoshop-wireframe we made

--- app/src/main/java/no/hiof/bo20_g28/stillashjelpen/MainActivity.java
+++ app/src/main/java/no/hiof/bo20_g28/stillashjelpen/MainActivity.java
@@ -4,21 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    private boolean loggedIn = false;
+    private FirebaseAuth firebaseAuth;
+    private TextView testText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (!loggedIn) {
-            Intent navigateToLoginActivityIntent = new Intent(this, LoginActivity.class);
-            Toast.makeText(getApplicationContext(), "Du er ikke innlogget.", Toast.LENGTH_SHORT).show();
-            startActivity(navigateToLoginActivityIntent);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        testText = findViewById(R.id.testText);
+
+        if (firebaseAuth.getCurrentUser() == null) {
+            Intent i = new Intent(this, LoginActivity.class);
+            startActivity(i);
         }
+        else{
+            testText.setText("Logget inn på email: \n" + firebaseAuth.getCurrentUser().getEmail());
+        }
+    }
+
+    public void logOutButtonClicked(View view) {
+        FirebaseAuth.getInstance().signOut();
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
     }
 }
