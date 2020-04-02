831fbcf66737644dbd30b1c6f81c81cf473b5cbf
Håkon Hjelmeland Dale <haakonhd@hiof.no>
2020-04-02 10:46:53 +0200

    Hello world!

---
+++ app/src/main/java/no/hiof/bo20_g28/stillashjelpen/MainActivity.java
@@ -0,0 +1,14 @@
+package no.hiof.bo20_g28.stillashjelpen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
