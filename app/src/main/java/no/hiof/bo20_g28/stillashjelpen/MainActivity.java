c1eae2d55058e5ffce49797cf727086dc807a97c
geir <geireilertsen@hotmail.com>
2020-04-03 12:08:44 +0200

    plus

--- app/src/main/java/no/hiof/bo20_g28/stillashjelpen/MainActivity.java
+++ app/src/main/java/no/hiof/bo20_g28/stillashjelpen/MainActivity.java
@@ -1,6 +1,7 @@
 package no.hiof.bo20_g28.stillashjelpen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import no.hiof.bo20_g28.stillashjelpen.model.Project;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,10 +10,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
+
     private TextView testText;
 
     @Override
@@ -21,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
 
         testText = findViewById(R.id.testText);
 
@@ -30,7 +36,21 @@ public class MainActivity extends AppCompatActivity {
         }
         else{
             testText.setText("Logget inn på email: \n" + firebaseAuth.getCurrentUser().getEmail());
+            testFirebaseDatabase();
         }
+
+    }
+
+    private void testFirebaseDatabase() {
+        Project p = new Project();
+
+        DatabaseReference projectRef = databaseReference.child("projects").push();
+
+        p.setProjectId(projectRef.getKey());
+        p.setUserId(firebaseAuth.getCurrentUser().getUid());
+        p.setProjectName("Møkkaløkka 21");
+
+        projectRef.setValue(p);
     }
 
     public void logOutButtonClicked(View view) {
---
+++ app/src/main/java/no/hiof/bo20_g28/stillashjelpen/model/Project.java
@@ -0,0 +1,67 @@
+package no.hiof.bo20_g28.stillashjelpen.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Project implements Serializable {
+
+    private String projectId, userId, projectName;
+    private ArrayList<Object> messages;
+    private ArrayList<Object> walls;
+    private Date dateCreated;
+
+
+    public Project() {
+
+    }
+
+
+    public String getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(String projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public ArrayList<Object> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(ArrayList<Object> messages) {
+        this.messages = messages;
+    }
+
+    public ArrayList<Object> getWalls() {
+        return walls;
+    }
+
+    public void setWalls(ArrayList<Object> walls) {
+        this.walls = walls;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+}
