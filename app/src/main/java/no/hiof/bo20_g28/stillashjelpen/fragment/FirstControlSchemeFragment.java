ec2747d3e22a8c9f56dc1ba7ee044cccb1679651
geir <geireilertsen@hotmail.com>
2020-05-08 14:18:37 +0200

    Added controlScheme fragment "Control Details"

--- app/src/main/java/no/hiof/bo20_g28/stillashjelpen/fragment/FirstControlSchemeFragment.java
+++ app/src/main/java/no/hiof/bo20_g28/stillashjelpen/fragment/FirstControlSchemeFragment.java
@@ -1,36 +1,152 @@
 package no.hiof.bo20_g28.stillashjelpen.fragment;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import no.hiof.bo20_g28.stillashjelpen.R;
+import no.hiof.bo20_g28.stillashjelpen.model.ControlScheme;
 import no.hiof.bo20_g28.stillashjelpen.model.Project;
 
 public class FirstControlSchemeFragment extends Fragment {
     public static final String ARG_OBJECT = "object";
 
-    TextView textView;
+    private EditText builderCompanyEditText, refNumEditText, placeEditText, builtByEditText, userCompanyEditText, phoneEditText,
+            contactPersonEditText, builderNameControlEditText, userNameControlEditText, builderControlDateEditText, userControlDateEditText;
+
     private View view;
 
+    private Button saveCsButton;
+
+
     private Project thisProject;
+    private ControlScheme thisCs;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_cs_first, container, false);
 
         Intent i = getActivity().getIntent();
         thisProject = (Project) i.getSerializableExtra("passedProject");
+        thisCs = thisProject.getControlScheme();
 
-        textView = view.findViewById(R.id.cs1TestTextView);
-        textView.setText(getResources().getString(R.string.tab_cs_first) + " for prosjekt: " + thisProject.getProjectName());
+        bindAllViews(view);
+        setOnClicks();
+
+        fillInPresetControlSchemeValuesForThisProject();
 
         return view;
+    }
+
+    public void fillInPresetControlSchemeValuesForThisProject(){
+        builderCompanyEditText.setText(thisCs.getBuilderCompanyName());
+        refNumEditText.setText(thisCs.getRefNum());
+        placeEditText.setText(thisCs.getPlaceName());
+        builtByEditText.setText(thisCs.getBuiltByName());
+        userCompanyEditText.setText(thisCs.getUserCompanyName());
+        phoneEditText.setText(thisCs.getUserPhoneNum());
+        contactPersonEditText.setText(thisCs.getContactPersonName());
+        builderNameControlEditText.setText(thisCs.getBuilderNameControlName());
+        userNameControlEditText.setText(thisCs.getUserNameControlName());
+        builderControlDateEditText.setText(thisCs.getBuilderControlDate());
+        userControlDateEditText.setText(thisCs.getUserControlDate());
+    }
+
+    public void updateThisProjectsControlScheme(){
+        thisProject.setControlScheme(thisCs);
+        DatabaseReference fDatabase = FirebaseDatabase.getInstance().getReference("projects");
+        DatabaseReference projectRef = fDatabase.child(thisProject.getProjectId());
+        projectRef.setValue(thisProject);
+    }
+
+    public void save(){
+        thisCs.setBuilderCompanyName(builderCompanyEditText.getText().toString().trim());
+        thisCs.setRefNum(refNumEditText.getText().toString().trim());
+        thisCs.setPlaceName(placeEditText.getText().toString().trim());
+        thisCs.setBuiltByName(builtByEditText.getText().toString().trim());
+        thisCs.setUserCompanyName(userCompanyEditText.getText().toString().trim());
+        thisCs.setUserPhoneNum(phoneEditText.getText().toString().trim());
+        thisCs.setContactPersonName(contactPersonEditText.getText().toString().trim());
+        thisCs.setBuilderNameControlName(builderNameControlEditText.getText().toString().trim());
+        thisCs.setUserNameControlName(userNameControlEditText.getText().toString().trim());
+        thisCs.setBuilderControlDate(builderControlDateEditText.getText().toString().trim());
+        thisCs.setUserControlDate(userControlDateEditText.getText().toString().trim());
+
+        updateThisProjectsControlScheme();
+        Toast.makeText(getContext(), "Kontroll-skjemaet er oppdatert",Toast.LENGTH_SHORT).show();
+    }
+
+    public void openDatePickerDialog(final View v) {
+
+        Calendar cal = Calendar. getInstance();
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(),
+                (view, year, monthOfYear, dayOfMonth) -> {
+                    String selectedDate = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
+                    switch (v.getId()) {
+                        case R.id.builderControlDateEditText:
+                            ((EditText)v).setText(selectedDate);
+                            break;
+                        case R.id.userControlDateEditText:
+                            ((EditText)v).setText(selectedDate);
+                            break;
+                    }
+                }, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+
+
+        datePickerDialog.getDatePicker().setMaxDate(cal.getTimeInMillis());
+        datePickerDialog.show();
+    }
+
+    public void bindAllViews(View v){
+        builderCompanyEditText = v.findViewById(R.id.builderCompanyEditText);
+        refNumEditText = v.findViewById(R.id.refNumEditText);
+        placeEditText = v.findViewById(R.id.placeEditText);
+        builtByEditText = v.findViewById(R.id.builtByEditText);
+        userCompanyEditText = v.findViewById(R.id.userCompanyEditText);
+        phoneEditText = v.findViewById(R.id.phoneEditText);
+        contactPersonEditText = v.findViewById(R.id.contactPersonEditText);
+        builderNameControlEditText = v.findViewById(R.id.builderNameControlEditText);
+        userNameControlEditText = v.findViewById(R.id.userNameControlEditText);
+        builderControlDateEditText = v.findViewById(R.id.builderControlDateEditText);
+        userControlDateEditText = v.findViewById(R.id.userControlDateEditText);
+
+        saveCsButton = v.findViewById(R.id.saveCsButton);
+    }
+
+    private void setOnClicks() {
+        builderControlDateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDatePickerDialog(v);
+            }
+        });
+        userControlDateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDatePickerDialog(v);
+            }
+        });
+
+        saveCsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                save();
+            }
+        });
     }
 
     @Override
--- app/src/main/java/no/hiof/bo20_g28/stillashjelpen/model/ControlScheme.java
+++ app/src/main/java/no/hiof/bo20_g28/stillashjelpen/model/ControlScheme.java
@@ -7,9 +7,21 @@ public class ControlScheme implements Serializable {
 
     private String controlSchemeId;
 
-
     private Date dateCreated;
     private Date dateChanged;
+
+    private String builderCompanyName;
+    private String refNum;
+    private String placeName;
+    private String builtByName;
+    private String userCompanyName;
+    private String userPhoneNum;
+    private String contactPersonName;
+    private String builderNameControlName;
+    private String userNameControlName;
+    private String builderControlDate;
+    private String userControlDate;
+
 
     public ControlScheme(String controlSchemeId) {
         this.controlSchemeId = controlSchemeId;
@@ -41,5 +53,93 @@ public class ControlScheme implements Serializable {
 
     public void setDateChanged(Date dateChanged) {
         this.dateChanged = dateChanged;
+    }
+
+    public String getBuilderCompanyName() {
+        return builderCompanyName;
+    }
+
+    public void setBuilderCompanyName(String builderCompanyName) {
+        this.builderCompanyName = builderCompanyName;
+    }
+
+    public String getRefNum() {
+        return refNum;
+    }
+
+    public void setRefNum(String refNum) {
+        this.refNum = refNum;
+    }
+
+    public String getPlaceName() {
+        return placeName;
+    }
+
+    public void setPlaceName(String placeName) {
+        this.placeName = placeName;
+    }
+
+    public String getBuiltByName() {
+        return builtByName;
+    }
+
+    public void setBuiltByName(String builtByName) {
+        this.builtByName = builtByName;
+    }
+
+    public String getUserCompanyName() {
+        return userCompanyName;
+    }
+
+    public void setUserCompanyName(String userCompanyName) {
+        this.userCompanyName = userCompanyName;
+    }
+
+    public String getUserPhoneNum() {
+        return userPhoneNum;
+    }
+
+    public void setUserPhoneNum(String userPhoneNum) {
+        this.userPhoneNum = userPhoneNum;
+    }
+
+    public String getContactPersonName() {
+        return contactPersonName;
+    }
+
+    public void setContactPersonName(String contactPersonName) {
+        this.contactPersonName = contactPersonName;
+    }
+
+    public String getBuilderNameControlName() {
+        return builderNameControlName;
+    }
+
+    public void setBuilderNameControlName(String builderNameControlName) {
+        this.builderNameControlName = builderNameControlName;
+    }
+
+    public String getUserNameControlName() {
+        return userNameControlName;
+    }
+
+    public void setUserNameControlName(String userNameControlName) {
+        this.userNameControlName = userNameControlName;
+    }
+
+    public String getBuilderControlDate() {
+        return builderControlDate;
+    }
+
+    public void setBuilderControlDate(String builderControlDate) {
+        this.builderControlDate = builderControlDate;
+    }
+
+    public String getUserControlDate() {
+        return userControlDate;
+    }
+
+    public void setUserControlDate(String userControlDate) {
+        this.userControlDate = userControlDate;
     }
 }
